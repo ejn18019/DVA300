1f4ef177f5e85aefde37ad9e2bf290cfdf69fc24
ejn18019 <84771623+ejn18019@users.noreply.github.com>
2021-05-25 10:53:00 +0200

    Update Getlinkspeed.m

--- Getlinkspeed.m
+++ Getlinkspeed.m
@@ -1,8 +1,8 @@
 function [User] = Getlinkspeed(User)
 %UNTITLED5 Summary of this function goes here
 %  This function sets a value to linkspeedand and returns the selected user with its linkspeed altered 
-%Linkspeed=(90*rand(1))+10;% this radomizes a linkspeed with a speed from 10-100mps
-Linkspeed = 100+(400*rand(1));
+Linkspeed=(90*rand(1))+10;% this radomizes a linkspeed with a speed from 10-100mps
+%Linkspeed = 100+(400*rand(1)); %This line is used when we increase the link speed from 100-500
 User.ls=Linkspeed;
 User.Lmax=Linkspeed;
 User.Lmin=0.1944; %hardcoded , will be dependent on data
